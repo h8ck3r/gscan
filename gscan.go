ae32441397c34b16fe255489640f32f0fd8a3cdf
H8CKR <h8ckr@protonmail.com>
2020-02-03 23:00:46 +0100

    fix concurrency

--- gscan.go
+++ gscan.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/pkg/errors"
 	"log"
 	"net"
 	"os"
 	"sort"
 	"strings"
+
+	//"strings"
 	"sync"
 	"syscall"
 
-	"github.com/pkg/errors"
+	//"github.com/pkg/errors"
 )
 
 var (
@@ -18,8 +22,8 @@ var (
 	hosts   []string
 	ports   []int
 	logger  = log.New(os.Stdout, "", 0)
-
-	hostResults   []*HostResult
+	errLogger  = log.New(os.Stderr, "", 1)
+	hostResults   []HostResult
 	hostWaitGroup sync.WaitGroup
 )
 
@@ -51,6 +55,8 @@ func generateHostList(hostArg string) ([]string, error) {
 		return strings.Split(hostArg, ","), nil
 	} else if strings.Contains(hostArg, "-") {
 		return nil, errors.Errorf("IP range definitions are not yet supported")
+	} else if strings.Contains(hostArg, "/") {
+		return getHostsForSubnet(hostArg)
 	} else {
 		addr := net.ParseIP(hostArg)
 		if addr == nil {
@@ -69,7 +75,8 @@ func generateHostList(hostArg string) ([]string, error) {
 }
 
 func validateArgs() {
-	if len(os.Args) < 2 {
+	flag.Parse()
+	if flag.NArg() < 1 {
 		_, _ = syscall.Write(2, []byte("Please specify at least one IP to scan\n"))
 		os.Exit(1)
 	}
@@ -80,19 +87,42 @@ func validateArgs() {
 		} else {
 			hostList, err := generateHostList(arg)
 			if err != nil {
-				panic(err)
+				errLogger.Fatal(err)
 			}
 			if hostList != nil {
 				hosts = append(hosts, hostList...)
 			}
+			sort.Strings(hosts)
 		}
 	}
 }
 
+func getHostsForSubnet(network string) ([]string, error) {
+	var ipList []string
+
+	ip, ipnet, err := net.ParseCIDR(network)
+	if err != nil {
+		return ipList, err
+	}
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); func(ip net.IP) net.IP {
+		for j := len(ip)-1; j>=0; j-- {
+			ip[j]++
+			if ip[j] > 0 {
+				break
+			}
+		}
+		return ip
+	}(ip) {
+		ipList = append(ipList, ip.String())
+	}
+	return ipList, nil
+}
+
 func portWorker(host string, port int, portResultChan chan *PortResult, portWaitGroup *sync.WaitGroup) {
 	address := fmt.Sprintf(host+":%d", port)
-	conn, err := net.Dial("tcp", address)
 	portResult := PortResult{}
+
+	conn, err := net.Dial("tcp", address)
 
 	if err != nil {
 		portResult.Port = port
@@ -109,45 +139,46 @@ func portWorker(host string, port int, portResultChan chan *PortResult, portWait
 	portWaitGroup.Done()
 }
 
-func hostWorker(host string, ports []int, hostResultChan chan *HostResult, hostWaitGroup *sync.WaitGroup, hostMutex *sync.Mutex) {
+func hostWorker(host string, ports []int, hostResultChan chan *HostResult, hostWaitGroup *sync.WaitGroup) {
 	logger.Printf("Initializing scan for %s\n", host)
 	portResultChan := make(chan *PortResult)
 	var portWaitGroup sync.WaitGroup
 	var hostResult HostResult
 
-	portWaitGroup.Add(len(ports))
+	hostResult.Host = host
+
 	for _, port := range ports {
+		portWaitGroup.Add(1)
 		go portWorker(host, port, portResultChan, &portWaitGroup)
 	}
 
 	for range ports {
-		select {
-		case portResult := <-portResultChan:
-			hostMutex.Lock()
-			if portResult.Port != 0 {
-				hostResult.OpenPorts = append(hostResult.OpenPorts, portResult.Port)
-			} else {
-				hostResult.ClosedPorts = append(hostResult.ClosedPorts, portResult.Port)
-			}
-			hostMutex.Unlock()
+		portResult := <-portResultChan
+		hostResult.Ports = append(hostResult.Ports, *portResult)
+		if portResult.State == Open {
+			hostResult.OpenPorts = append(hostResult.OpenPorts, portResult.Port)
+		} else {
+			hostResult.ClosedPorts = append(hostResult.ClosedPorts, portResult.Port)
 		}
 	}
 	portWaitGroup.Wait()
 	hostResultChan <- &hostResult
 	close(portResultChan)
 	hostWaitGroup.Done()
+	logger.Printf("Scanned %s\n", host)
 }
 
-func summary(hostResults []*HostResult) {
-
+func summary(hostResults []HostResult) {
 	for _, result := range hostResults {
 		sort.Ints(result.OpenPorts)
 		sort.Ints(result.ClosedPorts)
-		for _, port := range result.Ports {
-			if port.State == Open {
-				logger.Printf("Discovered open port %d on %s\n", port.Port, result.Host)
-			} else if port.State == Closed && verbose {
-				logger.Printf("Port %d on is %s\n", port.Port, result.Host)
+
+		for _, port := range result.OpenPorts {
+			logger.Printf("Discovered open port %d on %s\n", port, result.Host)
+		}
+		if verbose {
+			for _, port := range result.ClosedPorts {
+				logger.Printf("Port %d on is %s\n", port, result.Host)
 			}
 		}
 	}
@@ -158,29 +189,29 @@ func main() {
 
 	hostResultChan := make(chan *HostResult)
 
-	var hostMutex sync.Mutex
-	var hostResultMutex sync.Mutex
-
 	logger.Println("starting scan...")
 
-	for i := 1; i <= 1024; i++ {
+	for i := 1; i <= 100; i++ {
 		ports = append(ports, i)
 	}
 
 	for _, host := range hosts {
 		hostWaitGroup.Add(1)
-		go hostWorker(host, ports, hostResultChan, &hostWaitGroup, &hostMutex)
+		go hostWorker(host, ports, hostResultChan, &hostWaitGroup)
 	}
 
+	var hostResultWaitGroup sync.WaitGroup
+
 	for range hosts {
+		hostResultWaitGroup.Add(1)
 		go func() {
 			hostResult := <-hostResultChan
-			hostResultMutex.Lock()
-			hostResults = append(hostResults, hostResult)
-			hostResultMutex.Unlock()
+			hostResults = append(hostResults, *hostResult)
+			hostResultWaitGroup.Done()
 		}()
 	}
 	hostWaitGroup.Wait()
+	hostResultWaitGroup.Wait()
 	summary(hostResults)
 
 	close(hostResultChan)
