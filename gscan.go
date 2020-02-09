ba226b74a7d2837a486c216aaa97038ffec8c27e
H8CKR <h8ckr@protonmail.com>
2020-02-09 06:17:52 +0100

    WIP: fix concurrency

--- gscan.go
+++ gscan.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"github.com/pkg/errors"
@@ -9,6 +11,7 @@ import (
 	"os"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -22,8 +25,6 @@ var (
 	timeout time.Duration
 	logger  = log.New(os.Stdout, "", 0)
 	errLogger  = log.New(os.Stderr, "", 1)
-	hostResults   []*HostResult
-	hostWaitGroup sync.WaitGroup
 	hostArg string
 )
 
@@ -32,11 +33,30 @@ const (
 	Open   PortState = "open"
 )
 
+func init() {
+	logger.SetPrefix("")
+	flag.BoolVar(&verbose, "verbose", false, "verbose output")
+	flag.IntVar(&goroutineCap, "cap", 0, "the maximum number of parallel goroutines (0 is infinite)")
+	flag.DurationVar(&timeout, "timeout", 8000, "connection timeout (in milliseconds)")
+}
+
+type Scan struct {
+	Hosts []string
+	Ports []int
+	Protocol string
+	Timeout time.Duration
+	GoroutineCap int
+	WaitGroup *sync.WaitGroup
+	Results []HostResult
+}
+
+type Scanner interface {
+	scan() error
+}
+
 type HostResult struct {
 	Host        string
-	Ports       []PortResult
-	OpenPorts   []int
-	ClosedPorts []int
+	Ports       []*PortResult
 }
 
 type PortState string
@@ -46,16 +66,74 @@ type PortResult struct {
 	State PortState
 }
 
-func init() {
-	logger.SetPrefix("")
-	flag.BoolVar(&verbose, "verbose", false, "verbose output")
-	flag.IntVar(&goroutineCap, "cap", 0, "the maximum number of parallel goroutines (0 is infinite)")
-	flag.DurationVar(&timeout, "timeout", 4000, "connection timeout (in milliseconds)")
+func PortWorker(host string, port int, protocol string, timeout time.Duration, waitGroup *sync.WaitGroup, portResultChan chan *PortResult) {
+	var portResult PortResult
+	portResult.Port = port
+	conn, err := net.DialTimeout(protocol, fmt.Sprintf("%s:%d", host, port), timeout)
+	if err == nil {
+		portResult.State = Open
+		_ = conn.Close()
+	} else {
+		portResult.State = Closed
+	}
+	portResultChan <- &portResult
+	waitGroup.Done()
+}
+
+func HostWorker(host string, ports []int, protocol string, timeout time.Duration, goroutineCap *int, waitGroup *sync.WaitGroup, hostResultChan chan *HostResult) {
+	var portWaitGroup sync.WaitGroup
+	var portResults []*PortResult
+	var hostResult HostResult
+
+	var goroutineStack = 0
+
+	portResultChan := make(chan *PortResult, len(ports))
+
+	for _, port := range ports {
+		if *goroutineCap != 0 && goroutineStack % *goroutineCap == 0 {
+			portWaitGroup.Wait()
+		}
+		portWaitGroup.Add(1)
+		go PortWorker(host, port, protocol, timeout, &portWaitGroup, portResultChan)
+	}
+	for i := 0; i < cap(portResultChan); i++ {
+		select {
+		case portResult := <-portResultChan:
+			portResults = append(portResults, portResult)
+		}
+	}
+	portWaitGroup.Wait()
+	hostResult.Ports = portResults
+	hostResult.Host = host
+	hostResultChan <- &hostResult
+	close(portResultChan)
+	waitGroup.Done()
+}
+
+func (s *Scan) scan() error {
+	hostResultChan := make(chan *HostResult, len(hosts))
+
+	for _, host := range s.Hosts {
+		s.WaitGroup.Add(1)
+		go HostWorker(host, s.Ports, s.Protocol, s.Timeout, &s.GoroutineCap, s.WaitGroup, hostResultChan)
+	}
+	for i := 0; i < cap(hostResultChan); i++ {
+		select {
+		case hostResult := <-hostResultChan:
+			s.Results = append(s.Results, *hostResult)
+		}
+	}
+	s.WaitGroup.Wait()
+	close(hostResultChan)
+	return nil
 }
 
 func generateHostList(arg string) ([]string, error) {
 	if strings.Contains(arg, ",") {
-		return strings.Split(arg, ","), nil
+		hostStrings := strings.Split(arg, ",")
+		for _, host := range hostStrings {
+			hosts = append(hosts, host)
+		}
 	} else if regexp.MustCompile(`^([0-9]{1,3}(-[0-9]{1,3})?\.){3}[0-9]{1,3}(-[0-9]{1,3})?$`).MatchString(arg) {
 		return nil, errors.Errorf("IP range definitions are not yet supported")
 	} else if strings.Contains(arg, "/") {
@@ -63,18 +141,19 @@ func generateHostList(arg string) ([]string, error) {
 	} else {
 		addr := net.ParseIP(arg)
 		if addr == nil {
-			hostname, err := net.LookupHost(arg)
+			hostnames, err := net.LookupHost(arg)
 			if err != nil {
 				return nil, err
-			} else if hostname == nil {
+			} else if hostnames == nil {
 				return nil, errors.Errorf("failed to receive IP addresses or hostnames for %s", arg)
 			} else {
-				return hostname, nil
+				return hostnames, nil
 			}
 		} else {
 			return []string{addr.String()}, nil
 		}
 	}
+	return nil, nil
 }
 
 func validateArgs() {
@@ -88,7 +167,7 @@ func validateArgs() {
 	}
 
 	for _, arg := range os.Args[1:] {
-		if arg == "-v" || arg == "--verbose" || arg == "-verbose" {
+		if arg == "-verbose" || arg == "-timeout" || arg == "-cap" {
 			continue
 		} else {
 			hostArg = arg
@@ -96,125 +175,145 @@ func validateArgs() {
 			if err != nil {
 				errLogger.Fatal(err)
 			}
+			var hostStrings []string
 			if hostList != nil {
-				hosts = append(hosts, hostList...)
+				for _, host := range hostList {
+					hostStrings = append(hostStrings, host)
+				}
 			}
-			sort.Strings(hosts)
+			sort.Strings(hostStrings)
+			for _, host := range hostStrings {
+				hosts = append(hosts, host)
+			}
 		}
 	}
 }
 
-func getHostsForSubnet(network string) ([]string, error) {
-	var ipList []string
+func convertToBinary(ip net.IP) string {
+	bin := binary.BigEndian.Uint32(ip)
+	return strconv.FormatUint(uint64(bin), 2)
+}
 
-	ip, ipnet, err := net.ParseCIDR(network)
-	if err != nil {
-		return ipList, err
+func getBroadCastAddress(ipNet *net.IPNet) (net.IP, error) {
+	gateway := convertToBinary(ipNet.IP)
+	var broadcast net.IP
+
+	mask := ipNet.Mask
+	newMask := convertToBinary(net.IP(mask))
+	var broadcastBuf bytes.Buffer
+
+	for index, maskBit := range newMask {
+		uintMaskBit := uint32(maskBit)
+		bit := string(uintMaskBit)
+		if bit == "1" {
+			uintGatewayBit := uint32(gateway[index])
+			broadcastBuf.WriteString(string(uintGatewayBit))
+		} else {
+			broadcastBuf.WriteString("1")
+		}
 	}
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); func(ip net.IP) net.IP {
-		for j := len(ip)-1; j>=0; j-- {
+
+	if len(broadcastBuf.String()) != 32 {
+		panic("invalid ip size")
+	} else {
+		var broadcastBytes []string
+		broadcastBytes = append(broadcastBytes, broadcastBuf.String()[0:8])
+		broadcastBytes = append(broadcastBytes, broadcastBuf.String()[8:16])
+		broadcastBytes = append(broadcastBytes, broadcastBuf.String()[16:24])
+		broadcastBytes = append(broadcastBytes, broadcastBuf.String()[24:])
+
+		ip1, err := strconv.ParseInt(broadcastBytes[0], 2, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		ip2, err := strconv.ParseInt(broadcastBytes[1], 2, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		ip3, err := strconv.ParseInt(broadcastBytes[2], 2, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		ip4, err := strconv.ParseInt(broadcastBytes[3], 2, 64)
+		if err != nil {
+			panic(err)
+		}
+		broadcast = net.IPv4(byte(ip1), byte(ip2), byte(ip3), byte(ip4))
+	}
+
+	return broadcast, nil
+}
+
+func getHostsForSubnet(network string) ([]string, error) {
+	address, ipNet, err := net.ParseCIDR(network)
+	if err != nil {
+		return nil, err
+	}
+	broadcast, err := getBroadCastAddress(ipNet)
+	if err != nil {
+		panic(err)
+	}
+
+	var ipAddresses []string
+	for ip := address.Mask(ipNet.Mask); ipNet.Contains(ip); func(ip net.IP){
+		for j := len(ip) - 1; j >= 0; j-- {
 			ip[j]++
 			if ip[j] > 0 {
 				break
 			}
 		}
-		return ip
 	}(ip) {
-		ipList = append(ipList, ip.String())
-	}
-	return ipList, nil
-}
-
-func portWorker(host string, port int, portResultChan chan *PortResult, portWaitGroup *sync.WaitGroup) {
-	address := fmt.Sprintf(host+":%d", port)
-	var portResult = &PortResult{}
-	conn, err := net.DialTimeout("tcp", address, time.Millisecond * timeout)
-	if err != nil {
-		portResult.Port = port
-		portResult.State = Closed
-	} else {
-		logger.Printf("%s %s\n", address, Open)
-		portResult.Port = port
-		portResult.State = Open
-	}
-	if conn != nil {
-		_ = conn.Close()
-	}
-
-	portResultChan <- portResult
-	portWaitGroup.Done()
-}
-
-func hostWorker(host string, ports []int, hostResultChan chan *HostResult, hostWaitGroup *sync.WaitGroup) {
-	if verbose {
-		logger.Printf("Initializing scan for %s\n", host)
-	}
-	portResultChan := make(chan *PortResult, len(ports))
-	var portWaitGroup sync.WaitGroup
-	var hostResult HostResult
-
-	hostResult.Host = host
-
-	for _, port := range ports {
-		portWaitGroup.Add(1)
-		go portWorker(host, port, portResultChan, &portWaitGroup)
-	}
-
-	for range ports {
-		portResult := <-portResultChan
-		hostResult.Ports = append(hostResult.Ports, *portResult)
-		if portResult.State == Open {
-			hostResult.OpenPorts = append(hostResult.OpenPorts, portResult.Port)
-		} else {
-			hostResult.ClosedPorts = append(hostResult.ClosedPorts, portResult.Port)
+		if ip.String() != ipNet.IP.String() && ip.String() != broadcast.String() {
+			ipAddresses = append(ipAddresses, ip.String())
 		}
 	}
-	portWaitGroup.Wait()
-	hostResultChan <- &hostResult
-	close(portResultChan)
-	hostWaitGroup.Done()
+
+	return ipAddresses, nil
 }
 
-func summary(result *HostResult, hostResultWaitGroup *sync.WaitGroup) {
-	sort.Ints(result.OpenPorts)
-	sort.Ints(result.ClosedPorts)
-	for _, port := range result.OpenPorts {
-		logger.Printf("Discovered open port %d on %s\n", port, result.Host)
-	}
-	if verbose {
-		for _, port := range result.ClosedPorts {
-			logger.Printf("Port %d on %s is %s\n", port, result.Host, Closed)
+func summary(hostResults []HostResult) {
+	for _, hostResult := range hostResults {
+		for _, portResult := range hostResult.Ports {
+			if portResult.State == Open {
+				logger.Printf("Discovered open port %d on %s\n", portResult.Port, hostResult.Host)
+			} else if portResult.State == Closed && verbose {
+				logger.Printf("Port %d on %s is %s\n", portResult.Port, hostResult.Host, Closed)
+			}
 		}
 	}
-	hostResultWaitGroup.Done()
 }
 
 func main() {
 	validateArgs()
 
-	hostResultChan := make(chan *HostResult, len(hosts))
-
 	for i := 1; i <= 100; i++ {
 		ports = append(ports, i)
-
 	}
 	logger.Printf("Initializing scan for %s\n", hostArg)
-	for index, host := range hosts {
-		hostWaitGroup.Add(1)
-		go hostWorker(host, ports, hostResultChan, &hostWaitGroup)
-		if goroutineCap != 0 && index != 0 && index % goroutineCap == 0 {
-			hostWaitGroup.Wait()
-		}
+
+	var protocol = "tcp"
+	var scanWaitGroup sync.WaitGroup
+
+	sort.Strings(hosts)
+
+	scan := Scan{
+		Hosts:        hosts,
+		Ports:        ports,
+		Protocol:     protocol,
+		Timeout:      timeout,
+		GoroutineCap: goroutineCap,
+		WaitGroup:    &scanWaitGroup,
 	}
 
-	var hostResultWaitGroup sync.WaitGroup
-	for range hosts {
-		hostResultWaitGroup.Add(1)
-		go summary(<-hostResultChan, &hostResultWaitGroup)
+	var scanner Scanner = &scan
+
+	if err := scanner.scan(); err != nil {
+		panic(err)
 	}
-	hostWaitGroup.Wait()
-	hostResultWaitGroup.Wait()
-	close(hostResultChan)
+	summary(scan.Results)
 
 	logger.Printf("\nscan done.\n")
 }
