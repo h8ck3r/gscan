9cde66472676bb27532308f9612f7e78dc4abf34
H8CKR <h8ckr@protonmail.com>
2020-02-09 16:57:08 +0100

    WIP: fix concurrency

--- gscan.go
+++ gscan.go
@@ -2,26 +2,50 @@ package main
 
 import (
 	"github.com/h8ck3r/gscan/internal/cli"
+	"github.com/h8ck3r/gscan/internal/log"
 	"github.com/h8ck3r/gscan/pkg/types"
-	"log"
 )
 
 var (
 	scan types.Scan
 	scanner types.Scanner
 	ports []types.Port
+	verbose *bool
 )
 
 func main() {
 	cli.Parse()
-	log.Println(cli.Targets)
-	scan.Timeout = cli.Timeout
-	scan.GoroutineCap = cli.GoroutineCap
-	scan.Protocol = types.TCP
+	verbose = cli.GetVerbose()
+	var err error
+	scan.Targets, err = cli.GetTargets()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scan.Protocol = cli.GetProtocol()
+	scan.Timeout = cli.GetTimeout()
+	scan.GoroutineCap = cli.GetGoroutineCap()
+
 	for i := 1; i <= 100; i++ {
 		ports = append(ports, types.Port(i))
 	}
-	scan.Ports = ports
+	scan.Ports = cli.GetPorts()
+	scanner = &scan
+	hostResults, err := scanner.Scan()
+	if err != nil {
+		log.Error(err)
+	}
+	for _, result := range hostResults {
+		for _, portResult := range result.PortResults {
+			if portResult.State == types.Open {
+				log.Printf("discovered open port %d on %s\n", *portResult.Port, result.Host)
+			} else {
+				if *verbose {
+					log.Printf("port %d on %s is closed\n", *portResult.Port, result.Host)
+				}
+			}
+		}
+	}
 }
 
 /*
